c84f52cd6eba9d01eb847bb5341938526eed2d9c
Adam <adammonast@Adams-MBP.attlocal.net>
2020-01-08 19:13:49 -0500

    wednesday night merge

--- weight-journal/src/components/Dashboard.js
+++ weight-journal/src/components/Dashboard.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
-import { Link, useHistory, Route } from "react-router-dom";
-import UpdateWorkout from './UpdateWorkout'
+import { Link, useHistory } from "react-router-dom";
 
 function Dashboard(props) {
-  const [workouts, setWorkouts] = useState([]);
+  const [workout, setWorkout] = useState([]);
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get(`/users/${props.userid}/journal`)
+      .then(res => {
+        console.log(res.data);
+        setWorkout(res.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
 
   const handleDelete = e => {
     console.log(e.target.id);
@@ -16,7 +25,7 @@ function Dashboard(props) {
       .get(`users/${props.userid}/journal`)
       .then(res => {
         console.log(res.data);
-        props.setWorkout(res.data);
+        setWorkout(res.data);
       })
       .catch(err => console.log(err));
   };
@@ -28,23 +37,24 @@ function Dashboard(props) {
   return (
     <div>
       <Link to={`/userinfo/${props.userid}`}>Add User Info</Link>
-      {props.workouts.map(exercise => (
+      {workout.map(workouts => (
         // <Workout workouts={workouts} />
-        <div className="workout-container" key={exercise.id}>
+        <div className="workout-container" key={workouts.id}>
           <div className="workout-card">
             <div className="workout-items">
-              <p><span>Exercise: </span>{exercise.workout}</p>
-              <p><span>Target Muscles: </span>{exercise.body_region}</p>
-              <p><span>Weight: </span>{exercise.weight}</p>
-              <p><span>Reps: </span>{exercise.reps}</p>
-              <p><span>Notes: </span>{exercise.notes}</p>
+              <p><span>Exercise:  </span>{workouts.workout}</p>
+              <p><span>Target Muscles: </span> {workouts.body_region}</p>
+              <p><span>Weight: </span>{workouts.weight}</p>
+              <p><span>Reps: </span>{workouts.reps}</p>
+              <p><span>Sets: </span>{workouts.sets}</p>
+              <p>Notes: {workouts.notes}</p>
             </div>
             <div className="button-container">
-              <button className="workout-button" id={exercise.id} onClick={handleDelete}>
-                Delete
-              </button>
-              <button className="workout-button" id={exercise.id} onClick={handleEdit}>
+              <button className="workout-button" id={workouts.id} onClick={handleEdit}>
                 Edit
+              </button>
+              <button className="workout-button" id={workouts.id} onClick={handleDelete}>
+                Delete
               </button>
             </div>
           </div>
